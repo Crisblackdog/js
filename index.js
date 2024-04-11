d2a89d76ca8137ef4162a3bf9c168742fe57d720
Cristobal Sanchez Cruzat <127757410+Crisblackdog@users.noreply.github.com>
2024-04-11 10:23:01 -0400

    JS

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('hola mundo');
+// Tipos de datos en js
+//String: cadena de caracteres. 'a' 'hola' 'hola mundo'
+//boolean : True o False
+//null: nulo
+//Number : 12312312312312312 "123" 123
+//undefined. no esta definida
+//Object: objeto.
+
+//Definicion de variables
+//var 
+//let 
+//const 
+
+// var miPrimeraBariable = 'lala'
+
+let  = miPrimeraVariable = 'mi primera variable'
+console.log(miPrimeraVariable)
